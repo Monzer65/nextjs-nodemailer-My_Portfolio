7fea9e4f7d972cfde550c87d09a7bfd64ac3bdef
monzer <danesh.monzer@gmail.com>
2023-12-29 01:37:28 +0330

    updated sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,27 +1,39 @@
 export default function sitemap() {
   return [
     {
-      url: "https://monzerdanesh.com",
+      url: "https://www.monzerdanesh.ir",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://monzerdanesh.com/about",
+      url: "https://www.monzerdanesh.ir/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
+      priority: 0.7,
     },
     {
-      url: "https://monzerdanesh.com/contact",
+      url: "https://www.monzerdanesh.ir/contact",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
     },
     {
-      url: "https://monzerdanesh.com/projects",
+      url: "https://www.monzerdanesh.ir/projects",
       lastModified: new Date(),
       changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.monzerdanesh.ir/projects/Elevating%20User%20Experience%20with%20Next.js",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.monzerdanesh.ir/projects/Enhancing%20Shopping%20Experience%20with%20MERN%20Stack",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
       priority: 0.9,
     },
   ];
