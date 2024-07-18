28c1c63a0db1a6a44908672c39ac673c96d1791d
Monzer65 <105078122+Monzer65@users.noreply.github.com>
2024-07-18 17:28:46 +0330

    Update intro.jsx avatar

--- components/home/intro.jsx
+++ components/home/intro.jsx
@@ -10,7 +10,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Linkedin from "@/public/linkedin.svg";
 import Github from "@/public/github.svg";
-import avatar from "@/public/avatar.webp";
+import avatar from "@/public/photostudio_1721254692767.jpg";
 import { fira_code } from "../fonts";
 
 export default function Intro() {
